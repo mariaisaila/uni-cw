a991ac6e59d9f26fab054a499cddbae53a28f0f3
mariaisaila <87853613+mariaisaila@users.noreply.github.com>
2024-05-09 14:14:15 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ function toggleVisibilityByClass() {
     }
 }
 
-function addNewDiv(searchTable) {
+function addNewDiv(searchTable, item1, item2, item3, item4, item5, item6) {
     var newDiv = document.createElement('div');
     newDiv.style.display = 'inline-block';
     newDiv.style.margin = '5px';
@@ -25,48 +25,52 @@ function addNewDiv(searchTable) {
 
     if(searchTable==='People'){
         var newP = document.createElement('p');
-        newP.textContent = 'personid: ';
+        newP.textContent = 'personid: ' + item1;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
-        newP.textContent = 'name: ';
+        newP.textContent = 'name: ' + item2;
         newDiv.appendChild(newP);
         
         var newP = document.createElement('p');
-        newP.textContent = 'dob: ';
+        newP.textContent = 'address: ' + item3;
+        newDiv.appendChild(newP);
+        
+        var newP = document.createElement('p');
+        newP.textContent = 'dob: ' + item4;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
-        newP.textContent = 'licensenumber: ';
+        newP.textContent = 'licensenumber: ' + item5;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
-        newP.textContent = 'expirydate: ';
+        newP.textContent = 'expirydate: ' + item6;
         newDiv.appendChild(newP);
     }
     else{
         var newP = document.createElement('p');
-        newP.textContent = 'vehicleid: ';
+        newP.textContent = 'vehicleid: ' + item1;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
-        newP.textContent = 'make: ';
+        newP.textContent = 'make: ' + item2;
         newDiv.appendChild(newP);
         
         var newP = document.createElement('p');
-        newP.textContent = 'model: ';
+        newP.textContent = 'model: ' + item3;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
-        newP.textContent = 'colour: ';
+        newP.textContent = 'colour: ' + item4;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
-        newP.textContent = 'ownerid: ';
+        newP.textContent = 'ownerid: ' + item5;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
-        newP.textContent = 'name: ';
+        newP.textContent = 'name: ' + item6;
         newDiv.appendChild(newP);
 
         var newP = document.createElement('p');
@@ -90,8 +94,28 @@ async function fetchData(searchValue, searchField, searchTable) {
     } else if (data.length === 0) {
         document.getElementById('message').textContent = "No result found";
     } else {
+        if(searchTable==='People'){
+        data.forEach(item => {
+            var item1 = item.PeopleID;
+            var item2 = item.Name;
+            var item3 = item.Address;
+            var item4 = item.DOB;
+            var item5 = item.LicenseNumber;
+            var item6 = item.ExpiryDate;
+        });
+        }
+        else{
+            data.forEach(item => {
+                var item1 = item.VehicleID;
+                var item2 = item.Make;
+                var item3 = item.Model;
+                var item4 = item.Colour;
+                var item5 = item.OwnerID;
+                var item6="NULL";
+            });
+        }
         for(let i = 0; i<data.length; i++){
-            addNewDiv(searchTable);
+            addNewDiv(searchTable, item1, item2, item3, item4, item5, item6);
         }
         document.getElementById('message').textContent = "Search Successful";
     }
